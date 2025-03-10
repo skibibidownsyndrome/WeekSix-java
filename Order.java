a93cce54367e3d008042a4361909b4d2f93d3723
skibibidownsyndrome <lupug922@gmail.com>
2025-03-10 10:30:38 -0400

    Update Order.java

---
+++ Order.java
@@ -0,0 +1,31 @@
+//George Lupu 3/10/2025
+import java.util.*;
+public class Order {
+
+    public static void main(String[]args){
+
+        double burgers;
+        double fries;
+        double sodas;
+        double fullname;
+        double tax;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter full name: ");
+        fullname = input.nextInt();
+        System.out.print("Enter amount of burgers: ");
+        burgers = input.nextInt();
+        System.out.print("Enter amount od fries: ");
+        fries = input.nextInt();
+        System.out.print("Enter amount of sodas: ");
+        sodas = input.nextInt();
+
+
+
+
+
+
+    }
+
+}
